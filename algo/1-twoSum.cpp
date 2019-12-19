12b8434180076e97e6fd187adfd7012df2241b7e
jywu <wojoin98@gmail.com>
2019-12-18 21:55:15 -0500

    add 3Sum using binary search and two pointers

--- algo/1-twoSum.cpp
+++ algo/1-twoSum.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <unordered_map>
+#include <algorithm>
 
 using namespace std;
 using std::cout;
@@ -18,27 +19,58 @@ vector<int> twoSum(vector<int>& nums, int target) {
     //         }
     //     }
     // }
+
+    // using map for searching addend, T(n)=O(n)
+    //unordered_map<int, int> numsMap;
+    
+    // for(unsigned i = 0; i < nums.size(); ++i) {
+    //     numsMap.insert({nums[i], i});
+    // }
+    
+    // for(unsigned i = 0; i < nums.size(); ++i) {
+    //     int temp = target - nums[i];
+    //     unordered_map<int, int>::const_iterator got = numsMap.find(temp);
+
+    //     if( got != numsMap.end() &&  got->second != i ) {
+    //         res.push_back(i);
+    //         res.push_back(got->second);
+    //         return res;
+    //     }
+    // }
+
+    // using binary search , T(n)=O(nlgn)
+    // keep index of origin array
     unordered_map<int, int> numsMap;
     
     for(unsigned i = 0; i < nums.size(); ++i) {
         numsMap.insert({nums[i], i});
     }
+
+    sort(nums.begin(), nums.end());
     
     for(unsigned i = 0; i < nums.size(); ++i) {
-        int temp = target - nums[i];
-        unordered_map<int, int>::const_iterator got = numsMap.find(temp);
-
-        if( got != numsMap.end() &&  got->second != i ) {
-            res.push_back(i);
-            res.push_back(got->second);
+        int low = 0, high = nums.size() - 1;
+        
+        while(low <= high) {
+            if( nums[low] + nums[high] == target) {
+                res.push_back(numsMap.find(nums[low])->second);
+                res.push_back(numsMap.find(nums[high])->second);
+                return res;
+            } else if (low <= high && nums[low] + nums[high] > target) {
+                high--;
+            } else if (low <= high && nums[low] + nums[high] < target) {
+                low++;
+            }           
         }
+        
     }
+    
     return res;
 }
 
 int main(int argc, const char** argv) {
-    int myints[] = {3, 2, 7, 11, 16};
-    int target = 19;
+    int myints[] = {3, 3};
+    int target = 6;
 
     vector<int> fifth (myints, myints + sizeof(myints) / sizeof(int) );
 
