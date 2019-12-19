28f8e18c0fc99fe41357ecc277ece35dcff6acbc
jywu <wojoin98@gmail.com>
2019-12-19 06:38:42 -0500

    change 3Sum implementation

--- algo/15-threeSum.cpp
+++ algo/15-threeSum.cpp
@@ -8,51 +8,57 @@ using std::cout;
 using std::endl;
 
 vector<vector<int>> threeSum(vector<int>& nums) {
-    vector<vector<int>> res;
-    int target = 0;
-    sort(nums.begin(), nums.end());
+        vector<vector<int>> res;
+        if(nums.size()<3)
+            return res;
+        int target = 0;
+        sort(nums.begin(), nums.end());
 
-    for(int i = 0; i < nums.size(); ++i) {
-        // remove duplicate
-        if ( i > 0 && nums[i] == nums[i-1])
-            continue;
-            
-        int low = i + 1, high = nums.size() - 1;
-        while(low < high) {
-            int twoSum = nums[low] + nums[high];
-            if( twoSum == target - nums[i]) {
-                vector<int> v{nums[i], nums[low], nums[high]};
-                res.push_back(v);
+        for(int i = 0; i < nums.size(); ++i) { // i < nums.size()-2
+            // invalid
+            // nums[i] > 0 可得nums[high] >= nums[low] >= nums[i] > 0 即三者之和必大于0
+            if( nums[i] > 0)
+                continue;
 
-                low++;
-                high--;
-                while(low < high && nums[low] == nums[low - 1])
+            // remove duplicate
+            if ( i > 0 && (nums[i] == nums[i-1]))
+                continue;
+
+            int low = i + 1, high = nums.size() - 1;
+            while(low < high) {
+                int twoSum = nums[low] + nums[high];
+                if( twoSum == target - nums[i]) {
+                    vector<int> v{nums[i], nums[low], nums[high]};
+                    res.push_back(v);
+
                     low++;
-                while(low < high && nums[high] == nums[high + 1])
                     high--;
 
-                
-                
-                // for case num=[-2,0,0,2,2]
-                // for case num=[-2,0,3,-1,4,0,3,4,1,1,1,-3,-5,4,0]
-                // if( low < high && (nums[low] == nums[low+1] && nums[high] != nums[high-1]) ) {
-                //     low++;
-                // } else if(low < high && (nums[low] == nums[low+1] && nums[high] != nums[high-1]) ) { // for case num=[-4,-2,-2,-2,0,1,2,2,2,3,3,4,4,6,6]
-                //     high--;
-                // } else if(low < high && (nums[low] == nums[low+1] && nums[high] == nums[high-1])) {
-                //     low++;
-                //     high--;
-                // }
-            } else if(low < high && twoSum > target - nums[i]) {
-                high--;
-            } else {
-                low++;
+                    while(low < high && nums[low] == nums[low - 1])
+                        low++;
+                    while(low < high && nums[high] == nums[high + 1])
+                        high--;
+                    
+                    // for case num=[-2,0,0,2,2]
+                    // for case num=[-2,0,3,-1,4,0,3,4,1,1,1,-3,-5,4,0]
+                    // if( low < high && (nums[low] == nums[low+1] && nums[high] != nums[high-1]) ) {
+                    //     low++;
+                    // } else if(low < high && (nums[low] == nums[low+1] && nums[high] != nums[high-1]) ) { // for case num=[-4,-2,-2,-2,0,1,2,2,2,3,3,4,4,6,6]
+                    //     high--;
+                    // } else if(low < high && (nums[low] == nums[low+1] && nums[high] == nums[high-1])) {
+                    //     low++;
+                    //     high--;
+                    // }
+                } else if(low < high && twoSum > target - nums[i]) {
+                    high--;
+                } else {
+                    low++;
+                }
             }
         }
+        
+        return res;
     }
-    
-    return res;
-}
 
 int main(int argc, const char** argv) {
     //int myints[] = {-1, 0, 1, 2, -1, -4};
